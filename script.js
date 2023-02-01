cc50cee4778cb65b3418920c5df0caf3bbf83573
srineeth123 <86505976+srineeth123@users.noreply.github.com>
2023-02-01 09:28:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ chatForm.addEventListener('submit', (e) => {
 
   // create a new chat message element
   const chatMessage = document.createElement('div');
-  chatMessage.innerHTML = message;
+  chatMessage.innerHTML = `You: ${message}`;
 
   // add the chat message to the chat messages container
   chatMessages.appendChild(chatMessage);
 
   // clear the chat input
   chatInput.value = '';
+
+  // simulate a reply from another user
+  setTimeout(() => {
+    const reply = `Other user: Hi! How are you?`;
+
+    const replyMessage = document.createElement('div');
+    replyMessage.innerHTML = reply;
+
+    chatMessages.appendChild(replyMessage);
+  }, 1000);
 });
